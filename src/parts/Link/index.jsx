89eef366e6250eeec3021fdea695cbd2108667de
Adam Roberts <adamroberts@Adams-MacBook-Pro.local>
2024-07-10 16:17:31 +0100

    Link update

--- src/parts/Link/index.jsx
+++ src/parts/Link/index.jsx
@@ -9,9 +9,9 @@ import { Jacket } from './styles';
 
 // Component
 // ------------
-const Link = ({ icon, label, preText, link, rel, disabled }) => {
+const Link = ({ icon, label, preText, link, rel, disabled, onClick }) => {
     return (
-        <Jacket href={link} rel={rel} target={rel ? `_blank` : null} disabled={disabled}>
+        <Jacket href={link} rel={rel} target={rel ? `_blank` : null} disabled={disabled} onClick={onClick}>
             <span><Icon type={icon} /></span>
             {preText && disabled ? (
                 <div>
